e691d7efd62b4d46e72ee23c602288a09e1b4cda
Aaron <jerseypaint@gmail.com>
2020-07-23 12:30:51 -0400

    adding tilde to see effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SwiperCore, { Navigation } from 'swiper'
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import "swiper/swiper-bundle.min.css"
+import "~swiper/swiper-bundle.min.css"
 
 
 SwiperCore.use([Navigation])
@@ -18,7 +18,7 @@ const Container = styled.div`
 
 const HeroSection = styled.section`
   margin-top: -95px;
-  
+
   h1 {
     text-align: center;
     font-size: 14px;
