3e686b6c46c1571611b68b3ce377d2b3e6490265
Aaron <jerseypaint@gmail.com>
2020-07-22 21:45:14 -0400

    update site meta

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 
-import Header from "./header"
 import "./layout.css"
+
+const Main = styled.main`
+  max-width: 800px;
+  margin: 0 auto;
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,21 +30,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Main>{children}</Main>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,205 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
+import styled from "styled-components"
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Navigation } from 'swiper';
 
+import Img from "gatsby-image"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import 'swiper/swiper-bundle.min.css';
 
-const IndexPage = () => (
+
+SwiperCore.use([Navigation])
+
+const Container = styled.div`
+  padding:0 1rem;
+`
+
+const HeroSection = styled.section`
+  padding-bottom: 51px;
+
+  h1 {
+    text-align: center;
+    font-size: 14px;
+    line-height: 20px;
+    max-width: 285px;
+    margin: 0 auto;
+  }
+
+  .gatsby-image-wrapper {
+    height:400px;
+  }
+`
+
+const SliderSection = styled.section`
+  padding-bottom: 51px;
+
+  h2 {
+    font-size: 40px;
+  }
+
+  .swiper-container {
+      height: 800px;
+  }
+
+  .gatsby-image-wrapper {
+    height: 100%;
+  }
+
+  .swiper-button-next, .swiper-button-prev {
+    color: #3898EC;
+  }
+`
+
+const Form = styled.form`
+  padding-bottom: 51px;
+
+  h2 {
+    font-size: 14px;
+    text-align: center;
+    line-height: 16px;
+  }
+
+  label {
+    font-size: 12px;
+    line-height: 20px;
+  }
+
+  input[type="email"], input[type="text"] {
+    display: block;
+    background-color: #000;
+    border: 1px solid #ccc;
+    border-radius: 0px;
+    width: 100%;
+    height: 38px;
+    padding: 8px 12px;
+    margin-bottom: 10px;
+    font-size: 14px;
+    line-height: 1.42857143;
+
+    &:focus {
+      border: 1px solid #3898EC;
+      border-radius: 0;
+    }
+  }
+
+  input[type="submit"] {
+    padding: 6px 15px;
+    margin-top: 23px;
+    border: 1px white solid;
+    border-radius: 0px;
+    background-color: #fff;
+    color: #000;
+    text-transform: uppercase;
+  }
+
+  .hidden {
+    display: none;
+  }
+`
+
+const Hero = (props) => (
+  <HeroSection>
+    <Container>
+      <Img fluid={props.image} />
+      <h1>{props.description}</h1>
+    </Container>
+  </HeroSection>
+)
+
+const Slider = (props) => {
+
+  
+  return (
+    <SliderSection>
+      <Swiper
+        speed={700}
+        spaceBetween={0}
+        slidesPerView={1}
+        loop={true}
+        navigation
+      >
+        {props.slides.map( slide => (
+          <SwiperSlide>
+            <Container>
+              <h2>{slide.title}</h2>
+              <p>{slide.description}</p>
+            </Container>
+            <Img fluid={slide.image.fluid} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </SliderSection>
+  )
+}
+
+const ContactForm = (props) => {
+  return (
+    <section>
+      <Container>
+        <div id="mc_embed_signup">
+          <Form action="https://ockupy.us11.list-manage.com/subscribe/post?u=253768e3965b590e3a100c35d&amp;id=6c8e8c91e8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+                <h2>{props.title}</h2>
+                <label for="mce-FNAME">First Name </label>
+                <input type="text" value="" name="FNAME" id="mce-FNAME" />
+
+                <label for="mce-EMAIL">Email Address  <span>*</span></label>
+                <input type="email" value="" name="EMAIL" id="mce-EMAIL" />
+
+                <label for="mce-PHONE">Phone Number </label>
+                <input type="text" name="PHONE" value="" id="mce-PHONE" />
+
+                <div id="mce-responses">
+                  <div id="mce-error-response"></div>
+                  <div id="mce-success-response"></div>
+                </div>
+                <div aria-hidden="true" className="hidden"><input type="text" name="b_253768e3965b590e3a100c35d_6c8e8c91e8" tabindex="-1" value="" /></div>
+                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" />
+          </Form>
+        </div>
+      </Container>
+    </section>
+  )
+}
+
+
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Hero image={data.contentfulLandingPage.heroImage.fluid} description={data.contentfulLandingPage.heroDescription.heroDescription} />
+    <ContactForm title={data.contentfulLandingPage.formTitle} />
+    <Slider slides={data.contentfulLandingPage.slider} />
   </Layout>
 )
+
+export const query = graphql`
+  query HomePageQuery {
+    contentfulLandingPage {
+      formTitle
+      heroDescription {
+        heroDescription
+      }
+      heroImage {
+        fluid {
+          sizes
+          src
+          srcSet
+        }
+      }
+      slider {
+        title
+        description
+        image {
+          fluid {
+            src
+            srcSet
+            sizes
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
